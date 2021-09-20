be7249a87a18adf4ad33a0364cc3223f23839151
nicolas.mazia <nicolas.mazia@gmail.com>
2021-09-19 22:09:48 -0300

    firebase

--- mapfre-vida/src/environments/environment.ts
+++ mapfre-vida/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC1nSULMv_eGLJ9hmoS0uMko0nh8fkEc2Q",
+    authDomain: "mapfre-vida.firebaseapp.com",
+    projectId: "mapfre-vida",
+    storageBucket: "mapfre-vida.appspot.com",
+    messagingSenderId: "731005222085",
+    appId: "1:731005222085:web:104d0cd1ea98d6fd1b04ac",
+    measurementId: "G-3K22N4R79N"
+  }
 };
 
 /*
